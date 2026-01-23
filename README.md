4c217fbb038e35857dd9404b3d24f2eeb92191ac
lucaaszsx <jlucasmelob558@gmail.com>
2026-01-22 23:59:28 -0300

    Prob the last update for now

--- README.md
+++ README.md
@@ -88,34 +88,40 @@ make clean
 
 ## 🎨 Configuration
 
-You can customize the fire effect by modifying the constants and algorithm in `src/main.c`:
+You can customize the fire effect by modifying the constants in `src/main.c`:
 
 ### Display Settings
 
-| Constant           | Description                    | Default |
-|--------------------|--------------------------------|---------|
+| Constant           | Description                    | Default                         |
+|--------------------|--------------------------------|---------------------------------|
+| `APP_TITLE`        | Window title                   | "DOOM Fire Effect"              |
+| `DEBUG_FONT_PATH`  | Debug font path                | /assets/fonts/YOUR_FONT.ext     |
+| `TARGET_FPS`       | Targer frames per second       | 24                              |
 | `SCREEN_WIDTH`     | Window width in pixels         | 800                             |
 | `SCREEN_HEIGHT`    | Window height in pixels        | 400                             |
 | `MAP_ROWS`         | Number of vertical cells       | 80                              |
 | `MAP_COLUMNS`      | Number of horizontal cells     | 100                             |
 | `TARGET_FPS`       | Target frames per second       | 24.0                            |
 | `MAX_INTENSITY_LEVEL` | Maximum fire intensity      | 36                              |
-| `DEBUG_FONT_PATH`  | TTF debug font path            | /assets/fonts/YOUR_FONT.ttf     |
+| `FIRE_DECAY`       | Fire decay (vertical effect)   | 3                               |
+| `FIRE_SPREAD`      | Fire spread (horizontal effect)| 2                               |
 
-### Fire Propagation Settings
+### 🔥 Fire settings
 
-You can adjust the fire behavior in the `calculate_propagation()` function:
+You don't need to do changes to fire settings, but you can. If you want to see how the fire effect will behave with different decay and spread values, just change the `FIRE_DECAY` and `FIRE_SPREAD` constants.
 
-```c
-int vertical_decay = rand() % 3;
-int horizontal_decay = rand() % 2;
-```
+### ✏️ Font Settings
 
----
+If you're not going to use debug mode, you can ignore this topic. Otherwise, debug mode won't work without this configuration. If you want to use debug mode, you will need to use some font to render text. To configure the font, you can follow these steps:
+
+1. Download some font in TTF/OTF/TTC format
+2. Move the font file to the project folder
+3. Change the `DEBUG_FONT_PATH` at `src/main.c` to your font file path
+4. Follow the steps in topic **[Debug Mode](#debug-mode)** to configure debug mode
 
 ## 🐛 Debug Mode
 
-The project includes a debug mode that displays fire intensity values for each cell.
+The project includes a debug mode that displays fire intensity values for each cell. Isn't recommended to use debug mode when you have many cells. 
 
 **Enable/Disable:**
 
--- src/main.c
+++ src/main.c
@@ -6,16 +6,47 @@
 #include <stdio.h>
 #include <time.h>
 
-#define APP_TITLE           "DOOM Fire"
-#define SCREEN_WIDTH        800
-#define SCREEN_HEIGHT       400
-#define MAP_ROWS            80
-#define MAP_COLUMNS         100
-#define TARGET_FPS          24.0
-#define MAX_INTENSITY_LEVEL 36
-#define DEBUG_FONT_PATH     "./assets/fonts/YOUR_FONT.ttf"
-//#define DEBUG
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+// #define DEBUG
 
+// Constants
+const char *APP_TITLE       = "DOOM Fire Effect";
+const char *DEBUG_FONT_PATH = "./assets/fonts/YOUR_FONT.ext";
+const double TARGET_FPS        = 24.0;
+
+enum {
+    SCREEN_WIDTH        = 800,
+    SCREEN_HEIGHT       = 400,
+    MAP_ROWS            = 80,
+    MAP_COLUMNS         = 100,
+    MAX_INTENSITY_LEVEL = 36,
+    FIRE_DECAY          = 3,
+    FIRE_SPREAD         = 2
+};
+
+const double TARGET_FRAME_TIME = 1.0 / TARGET_FPS;
+
+const int TOTAL_MAP_SIZE = MAP_ROWS * MAP_COLUMNS;
+const int CELL_WIDTH     = SCREEN_WIDTH / MAP_COLUMNS;
+const int CELL_HEIGHT    = SCREEN_HEIGHT / MAP_ROWS;
+
+const SDL_Color DEBUG_RECT_COLOR   = { 0, 0, 0, 0 };
+const SDL_Color DEBUG_STROKE_COLOR = { 0, 0, 255, 255 };
+const SDL_Color DEBUG_TEXT_COLOR   = { 255, 255, 255, 255 };
+
+const SDL_Color FIRE_INTENSITY[MAX_INTENSITY_LEVEL + 1] = {
+    { 7, 7, 7, 255 }, { 31, 7, 7, 255 }, { 47, 15, 7, 255 }, { 71, 15, 7, 255 }, { 87, 23, 7, 255 },
+    { 103, 31, 7, 255 }, { 119, 31, 7, 255 }, { 143, 39, 7, 255 }, { 159, 47, 7, 255 },
+    { 175, 63, 7, 255 }, { 191, 71, 7, 255 }, { 199, 71, 7, 255 }, { 223, 79, 7, 255 },
+    { 223, 87, 7, 255 }, { 223, 87, 7, 255 }, { 215, 95, 7, 255 }, { 215, 95, 7, 255 },
+    { 215, 103, 15, 255 }, { 207, 111, 15, 255 }, { 207, 119, 15, 255 }, { 207, 127, 15, 255 },
+    { 207, 135, 23, 255 }, { 199, 135, 23, 255 }, { 199, 143, 23, 255 }, { 199, 151, 31, 255 },
+    { 191, 159, 31, 255 }, { 191, 159, 31, 255 }, { 191, 167, 39, 255 }, { 191, 167, 39, 255 },
+    { 191, 175, 47, 255 }, { 183, 175, 47, 255 }, { 183, 183, 47, 255 }, { 183, 183, 55, 255 },
+    { 207, 207, 111, 255 }, { 223, 223, 159, 255 }, { 239, 239, 199, 255 }, { 255, 255, 255, 255 }
+};
+
+// Structs
 typedef struct {
     SDL_Color color;
     float width;
@@ -38,46 +69,27 @@ typedef struct {
 } TextCache;
 #endif
 
-const SDL_Color fire_intensity[MAX_INTENSITY_LEVEL + 1] = {
-    { 7, 7, 7, 255 }, { 31, 7, 7, 255 }, { 47, 15, 7, 255 }, { 71, 15, 7, 255 }, { 87, 23, 7, 255 },
-    { 103, 31, 7, 255 }, { 119, 31, 7, 255 }, { 143, 39, 7, 255 }, { 159, 47, 7, 255 },
-    { 175, 63, 7, 255 }, { 191, 71, 7, 255 }, { 199, 71, 7, 255 }, { 223, 79, 7, 255 },
-    { 223, 87, 7, 255 }, { 223, 87, 7, 255 }, { 215, 95, 7, 255 }, { 215, 95, 7, 255 },
-    { 215, 103, 15, 255 }, { 207, 111, 15, 255 }, { 207, 119, 15, 255 }, { 207, 127, 15, 255 },
-    { 207, 135, 23, 255 }, { 199, 135, 23, 255 }, { 199, 143, 23, 255 }, { 199, 151, 31, 255 },
-    { 191, 159, 31, 255 }, { 191, 159, 31, 255 }, { 191, 167, 39, 255 }, { 191, 167, 39, 255 },
-    { 191, 175, 47, 255 }, { 183, 175, 47, 255 }, { 183, 183, 47, 255 }, { 183, 183, 55, 255 },
-    { 207, 207, 111, 255 }, { 223, 223, 159, 255 }, { 239, 239, 199, 255 }, { 255, 255, 255, 255 }
-};
-
-const int total_map_size = MAP_ROWS * MAP_COLUMNS;
-
-const int debug_cell_width = SCREEN_WIDTH / MAP_COLUMNS;
-const int debug_cell_height = SCREEN_HEIGHT / MAP_ROWS;
-
-const SDL_Color debug_rect_color = { 0, 0, 0, 0 };
-const SDL_Color debug_stroke_color = { 0, 0, 255, 255 };
-const SDL_Color debug_text_color = { 255, 255, 255, 255 };
-
-const double target_frame_time = 1.0 / TARGET_FPS;
-
 /**
  * Initializes SDL3, SDL3_ttf and creates window, renderer and font
  * Exits program if initialization fails
  */
-void init_sdl(SDL_Window **window, SDL_Renderer **renderer, TTF_Font **font);
+void init_sdl(SDL_Window **window, SDL_Renderer **renderer
+              #ifdef DEBUG
+              , TTF_Font **font
+              #endif
+);
 
 /**
  * Allocates and initializes fire map with base intensity values
  * Bottom row is set to MAX_INTENSITY_LEVEL, others to 0
  */
-void create_base_map(uint8_t **fire_map);
+void create_base_map(int **fire_map);
 
 /**
  * Calculates fire propagation for each frame
  * Applies vertical and horizontal decay with randomness
  */
-void calculate_propagation(uint8_t *fire_map);
+void calculate_propagation(int *fire_map);
 
 /**
  * Renders a custom rectangle with stroke support
@@ -98,21 +110,21 @@ void destroy_text_cache(TextCache *cache);
 /**
  * Renders fire with debug information using cached textures
  */
-void render_fire(SDL_Renderer *renderer, uint8_t *fire_map, TextCache *cache);
+void render_fire(SDL_Renderer *renderer, int *fire_map, TextCache *cache);
 #else
 /**
  * Renders fire without debug information
  */
-void render_fire(SDL_Renderer *renderer, uint8_t *fire_map);
+void render_fire(SDL_Renderer *renderer, int *fire_map);
 #endif
 
 /**
  * Cleans up all resources before closing application
  */
-void close_app(SDL_Window *window, SDL_Renderer *renderer, uint8_t *fire_map, 
-               TTF_Font *font
+void close_app(SDL_Window *window, SDL_Renderer *renderer, int *fire_map
                #ifdef DEBUG
-               , TextCache *cache
+               , TTF_Font *font,
+               TextCache *cache
                #endif
 );
 
@@ -123,18 +135,24 @@ int main(int argc, char **argv) {
     SDL_Renderer *renderer;
     SDL_Event event;
     bool running = true;
-
-    TTF_Font *font;
-
-    init_sdl(&window, &renderer, &font);
-    
-    uint8_t *fire_map;
-    create_base_map(&fire_map);
     
     #ifdef DEBUG
+    TTF_Font *font;
     TextCache text_cache;
+    #endif
+
+    init_sdl(&window, &renderer
+        #ifdef DEBUG
+        , &font
+        #endif
+    );
+
+    #ifdef DEBUG
     init_text_cache(&text_cache, renderer, font);
     #endif
+
+    int *fire_map;
+    create_base_map(&fire_map);
     
     Uint64 freq = SDL_GetPerformanceFrequency();
 
@@ -167,28 +185,33 @@ int main(int argc, char **argv) {
         Uint64 frame_end = SDL_GetPerformanceCounter();
         double frame_time = (double)(frame_end - frame_start) / (double)freq;
 
-        if (frame_time < target_frame_time) {
-            double delay_seconds = target_frame_time - frame_time;
+        if (frame_time < TARGET_FRAME_TIME) {
+            double delay_seconds = TARGET_FRAME_TIME - frame_time;
             SDL_Delay((Uint32)(delay_seconds * 1000.0));
         }
     }
 
-    close_app(window, renderer, fire_map, font
+    close_app(window, renderer, fire_map
               #ifdef DEBUG
-              , &text_cache
+              , font, &text_cache
               #endif
     );
 
     return 0;
 }
 
-void init_sdl(SDL_Window **window, SDL_Renderer **renderer,
-               TTF_Font **font) {
+void init_sdl(SDL_Window **window, SDL_Renderer **renderer
+              #ifdef DEBUG
+              , TTF_Font **font
+              #endif
+) {
     if (!SDL_CreateWindowAndRenderer(APP_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT,
                                     0, window, renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         exit(1);
     }
+
+    #ifdef DEBUG
     if (!TTF_Init()) {
         SDL_Log("Couldn't initialize the SDL3 TTF: %s", SDL_GetError());
         exit(1);
@@ -200,45 +223,46 @@ void init_sdl(SDL_Window **window, SDL_Renderer **renderer,
         SDL_Log("Couldn't open font: %s\n", SDL_GetError());
         exit(1);
     }
+    #endif
 }
 
-void create_base_map(uint8_t **fire_map) {
-    uint8_t *new_fire_map = malloc(total_map_size);
+void create_base_map(int **fire_map) {
+    int *new_fire_map = malloc(TOTAL_MAP_SIZE * sizeof(int));
 
     if (!new_fire_map) {
         SDL_Log("Failed to allocate fire map");
         exit(1);
     }
 
-    for (int i = 0; i < total_map_size; i++)
-        new_fire_map[i] = i >= total_map_size - MAP_COLUMNS
+    for (int i = 0; i < TOTAL_MAP_SIZE; i++)
+        new_fire_map[i] = i >= TOTAL_MAP_SIZE - MAP_COLUMNS
             ? MAX_INTENSITY_LEVEL
             : 0;
     
     *fire_map = new_fire_map;
 }
 
-void calculate_propagation(uint8_t *fire_map) {
-    for (int k = 0; k < MAP_COLUMNS; k++) {
-        for (int j = 0; j < MAP_ROWS; j++) {
+void calculate_propagation(int *fire_map) {
+    for (int j = 0; j < MAP_ROWS - 1; j++) {
+        for (int k = 0; k < MAP_COLUMNS; k++) {
             int current_index = k + j * MAP_COLUMNS;
-            int below_index = current_index + MAP_COLUMNS;
+            int below_index   = current_index + MAP_COLUMNS;
+            int below_intensity = fire_map[below_index];
 
-            if (below_index >= total_map_size)
+            if (below_intensity == 0) {
+                fire_map[current_index] = 0;
                 continue;
-
-            int vertical_decay = rand() % 3;
-            int horizontal_decay = rand() % 2;
-            uint8_t below_fire_intensity = fire_map[below_index];
-            uint8_t new_fire_intensity = (uint8_t)(below_fire_intensity - vertical_decay) > MAX_INTENSITY_LEVEL
-                                            ? 0
-                                            : below_fire_intensity - vertical_decay;
-            int expected_index = current_index - horizontal_decay;
-            int pixel_to_change = expected_index < 0
-                ? 0
-                : expected_index;
+            }
             
-            fire_map[pixel_to_change] = new_fire_intensity;
+            int decay = rand() % (FIRE_DECAY + 1);
+            int spread = rand() % (FIRE_SPREAD + 1);
+            
+            int new_intensity = MAX(below_intensity - decay, 0);
+            
+            int new_col = MAX(k - spread, 0);
+            int target_index = new_col + j * MAP_COLUMNS;
+            
+            fire_map[target_index] = new_intensity;
         }
     }
 }
@@ -280,7 +304,7 @@ void init_text_cache(TextCache *cache, SDL_Renderer *renderer, TTF_Font *font) {
         snprintf(intensity_text, sizeof(intensity_text), "%d", i);
         
         SDL_Surface *text_surface = TTF_RenderText_Blended(font, intensity_text, 0,
-                                                           debug_text_color);
+                                                           DEBUG_TEXT_COLOR);
         
         if (!text_surface) {
             SDL_Log("Failed to render text for intensity %d: %s", i, SDL_GetError());
@@ -309,15 +333,15 @@ void destroy_text_cache(TextCache *cache) {
     }
 }
 
-void render_fire(SDL_Renderer *renderer, uint8_t *fire_map, TextCache *cache) {
+void render_fire(SDL_Renderer *renderer, int *fire_map, TextCache *cache) {
     CustomFRect debug_rect = {
-        .color = debug_rect_color,
-        .w = debug_cell_width,
-        .h = debug_cell_height,
+        .color = DEBUG_RECT_COLOR,
+        .w = CELL_WIDTH,
+        .h = CELL_HEIGHT,
         .x = 0,
         .y = 0,
         .stroke = {
-            .color = debug_stroke_color,
+            .color = DEBUG_STROKE_COLOR,
             .width = 1
         }
     };
@@ -328,8 +352,8 @@ void render_fire(SDL_Renderer *renderer, uint8_t *fire_map, TextCache *cache) {
         for (int j = 0; j < MAP_ROWS; j++) {
             int index = j * MAP_COLUMNS + k;
             int intensity = fire_map[index];
-            int rx = debug_cell_width * k;
-            int ry = debug_cell_height * j;
+            int rx = CELL_WIDTH * k;
+            int ry = CELL_HEIGHT * j;
 
             debug_rect.x = rx;
             debug_rect.y = ry;
@@ -340,8 +364,8 @@ void render_fire(SDL_Renderer *renderer, uint8_t *fire_map, TextCache *cache) {
             if (text_texture) {
                 text_dst_rect.w = cache->widths[intensity];
                 text_dst_rect.h = cache->heights[intensity];
-                text_dst_rect.x = rx + (debug_cell_width - text_dst_rect.w) / 2.0f;
-                text_dst_rect.y = ry + (debug_cell_height - text_dst_rect.h) / 2.0f;
+                text_dst_rect.x = rx + (CELL_WIDTH - text_dst_rect.w) / 2.0f;
+                text_dst_rect.y = ry + (CELL_HEIGHT - text_dst_rect.h) / 2.0f;
 
                 render_rect(renderer, debug_rect);
                 SDL_RenderTexture(renderer, text_texture, NULL, &text_dst_rect);
@@ -352,17 +376,17 @@ void render_fire(SDL_Renderer *renderer, uint8_t *fire_map, TextCache *cache) {
     }
 }
 #else
-void render_fire(SDL_Renderer *renderer, uint8_t *fire_map) {
+void render_fire(SDL_Renderer *renderer, int *fire_map) {
     for (int k = 0; k < MAP_COLUMNS; k++) {
         for (int j = 0; j < MAP_ROWS; j++) {
             int index = j * MAP_COLUMNS + k;
-            SDL_Color color = fire_intensity[fire_map[index]];
+            SDL_Color color = FIRE_INTENSITY[fire_map[index]];
             
             SDL_FRect cell_rect = {
-                .x = debug_cell_width * k,
-                .y = debug_cell_height * j,
-                .w = debug_cell_width,
-                .h = debug_cell_height
+                .x = CELL_WIDTH * k,
+                .y = CELL_HEIGHT * j,
+                .w = CELL_WIDTH,
+                .h = CELL_HEIGHT
             };
             
             SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
@@ -373,22 +397,25 @@ void render_fire(SDL_Renderer *renderer, uint8_t *fire_map) {
 #endif
 
 void close_app(SDL_Window *window, SDL_Renderer *renderer,
-               uint8_t *fire_map, TTF_Font *font
+               int *fire_map
                #ifdef DEBUG
-               , TextCache *cache
+               , TTF_Font *font,
+               TextCache *cache
                #endif
 ) {
     #ifdef DEBUG
     destroy_text_cache(cache);
+
+    if (font) TTF_CloseFont(font);
     #endif
     
-    if (font) TTF_CloseFont(font);
-
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
     free(fire_map);
 
+    #ifdef DEBUG
     TTF_Quit();
+    #endif
     SDL_Quit();
 }
